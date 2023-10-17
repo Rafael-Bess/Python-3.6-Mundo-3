bf436ac736d5e01d91d875afd269c221d47ea1c4
Rafael Bess <140470059+Rafael-Bess@users.noreply.github.com>
2023-10-16 22:28:43 -0300

    Delete Python-3.6 - Fase 17.4/teste.py

--- Python-3.6 - Fase 17.4/teste.py
+++
@@ -1,9 +0,0 @@
-def mensagem(msg): # utilizei o nome de msg para definir o parametro do 2° print onde mensagem('vai ir pra dentro do print de msg')
-    print('-=' * 20)
-    print(msg)
-    print('-=' * 20)
-
-mensagem('Teste')
-
-
-
