3e4b72dc260ded25a489e5d9919dd0acb60dde9f
Rafael Bess <140470059+Rafael-Bess@users.noreply.github.com>
2023-10-16 21:29:28 -0300

    Add files via upload

---
+++ Python-3.6 - Fase 17.4/Praticando funções.py.py
@@ -0,0 +1,9 @@
+def mensagem(msg): # utilizei o nome de msg para definir o parametro do 2° print onde mensagem('vai ir pra dentro do print de msg')
+    print('-=' * 20)
+    print(msg)
+    print('-=' * 20)
+
+mensagem('Teste')
+
+
+
